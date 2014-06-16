1d5f1e220124204935ab659d08f8f76e2260dc69
marioscience@hotmail.com <aLA2cf&3feb>
2014-06-16 13:05:59 -0600

    Project restructured to have doc and src folders. Added spring runoff document. Fixed minor issues.

--- src/iUTAHData/settings/base.py
+++ src/iUTAHData/settings/base.py
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'iUTAHData.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'db.sqlite3',
     }
 }
 
