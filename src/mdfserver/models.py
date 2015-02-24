c6788db98a9b8e04c7f75a1ce66fa085c8815d7f
Mario <aLA2cf&3feb>
2015-02-24 11:59:02 -0700

    - Changed max size for tinyMCE fields to 20,000 characters.

--- src/mdfserver/models.py
+++ src/mdfserver/models.py
@@ -6,14 +6,14 @@ from tinymce import models as tinymce_models
 class Page(models.Model):
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
-    content = models.TextField(max_length=8000) #tinymce_models.HTMLField()#forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30})) #Use the WYSIWYG editor in this field.
+    content = models.TextField(max_length=20000) #tinymce_models.HTMLField()#forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30})) #Use the WYSIWYG editor in this field.
     def __unicode__(self):
         return self.title
 
 class Subpage(models.Model):
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
-    content = models.TextField(max_length=8000)#tinymce_models.HTMLField() #forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30})) #Use the WYSIWYG editor in this field.
+    content = models.TextField(max_length=20000)#tinymce_models.HTMLField() #forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30})) #Use the WYSIWYG editor in this field.
     pages = models.ForeignKey(Page)
     def __unicode__(self):
         return self.title
