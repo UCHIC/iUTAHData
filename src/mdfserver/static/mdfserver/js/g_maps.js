261018f5613be7e1156207532de0f10f7316c05f
Mario <marioscience@hotmail.com>
2015-04-22 13:27:15 -0600

    Changed Maps to be of type terrain.

--- src/mdfserver/static/mdfserver/js/g_maps.js
+++ src/mdfserver/static/mdfserver/js/g_maps.js
@@ -1,8 +1,8 @@
 function initialize(lat, long) {
     var mapOptions = {
         center: new google.maps.LatLng(lat, long),
-        zoom: 16,
-        mapTypeId: google.maps.MapTypeId.SATELLITE
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.TERRAIN
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
