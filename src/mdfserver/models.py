1aef86e526500d8dac3dd3c146a88dadcef3f9f6
marioscience <marioscience@hotmail.com>
2015-07-30 21:52:25 -0600

    Setup sub pages that are not in nav bar. They are accessible only through links.

--- src/mdfserver/models.py
+++ src/mdfserver/models.py
@@ -13,6 +13,7 @@ class Page(models.Model):
 class Subpage(models.Model):
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
+    url_visible = models.BooleanField()
     content = models.TextField(max_length=20000)#tinymce_models.HTMLField() #forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30})) #Use the WYSIWYG editor in this field.
     pages = models.ForeignKey(Page)
     def __unicode__(self):
