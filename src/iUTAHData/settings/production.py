96ad1928d77526b92409e7907e36761e15213742
darksinge <cr.blackburn89@gmail.com>
2018-05-07 16:35:59 -0600

    updated settings

--- src/iUTAHData/settings/production.py
+++ src/iUTAHData/settings/production.py
@@ -11,10 +11,14 @@ TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', data['host']]
 
+# On production, SITE_ROOT is the parent directory of the project (e.i. not /path/to/project/src)
 SITE_ROOT = os.environ['APPL_PHYSICAL_PATH']
 SITE_URL = os.environ['APPL_VIRTUAL_PATH'] + "/"
 
-STATIC_ROOT = os.path.join(SITE_ROOT, 'static')
+# NOTE: STATIC_ROOT on production is not located inside the project directory
+STATIC_ROOT = os.path.join(SITE_ROOT, 'mdfserver', 'static')
 STATIC_URL = SITE_URL + 'static/'
+
+BASE_URL = 'mdf/'
 
 django.setup()
